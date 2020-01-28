d71dcb93c686eb6ded1445bf12595bf941d2ae21
elenabaurkot <elenabaurkot@gmail.com>
2020-01-28 00:02:07 -0500

    questions array created

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 //  start array
 var questions = [
     {   
-    q: "",
-    a: ""
-    },
+    q: "What do the 100 folds in a chef's hat represent?",
+    a: [ 
+        {text: '', correct: false},
+        {text: '', correct: false},
+        {text: '100 ways to cook an egg', correct: true},
+        {text: '', correct: false},
+    ]},
     {
-
-    },
+    q: "What is the national animal of Scotland?",
+    a: [
+        {text: 'Unicorn', correct: true},
+        {text: '', correct: false},
+        {text: '', correct: false},
+        {text: '', correct: false},
+    ]},
     {
-
-    },
+    q: "On average, how much does the heart of a blue whale (the world's largest animal) weight?",
+    a: [ 
+        {text: '', correct: false},
+        {text: '', correct: false},
+        {text: '', correct: false},
+        {text: '400 pounds', correct: true},
+    ]},
     {
-
-    },
+    q: "Which bird can mimick almost any sound they hear (including a chainsaw)?",
+    a: [
+        {text: '', correct: false},
+        {text: '', correct: false},
+        {text: 'lyrebird', correct: true},
+        {text: '', correct: false},
+    ]},
     {
-
-    }
+    q: "Which college can you major in wine ('Viticulture and Enology' a.k.a, the cultivation of grapes and the science of winemaking)",
+    a: [
+        {text: 'University of California,  Santa Barbara', correct: false},
+        {text: 'Cornell University', correct: true},
+        {text: 'Arizona State University', correct: false},
+        {text: 'Tulane', correct: false},
+    ]},
+    {
+    q: "How much does it cost to make a $1 bill?",
+    a: [
+        {text: '1 cent', correct: false},
+        {text: '5 cents', correct: true},
+        {text: '8 cents', correct: false},
+        {text: '11 cents', correct: false},
+    ]},
 ]
